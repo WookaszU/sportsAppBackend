acb0dfcf6a2ab27d646615391b8a514ef8f3780b
WookaszU <l.uchman@interia.pl>
2018-10-08 21:50:11 +0200

    Added missing import.

--- src/main/java/pl/edu/agh/sportsApp/controller/UserController.java
+++ src/main/java/pl/edu/agh/sportsApp/controller/UserController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
