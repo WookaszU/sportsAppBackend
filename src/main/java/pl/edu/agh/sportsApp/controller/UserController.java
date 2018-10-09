1dbd65d5a7c69e0291bc79991e5b310395fece6f
WookaszU <l.uchman@interia.pl>
2018-10-09 17:56:06 +0200

    Added missed response codes in controllers.

--- src/main/java/pl/edu/agh/sportsApp/controller/UserController.java
+++ src/main/java/pl/edu/agh/sportsApp/controller/UserController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ final class UserController {
     }
 
     @ApiOperation(value="Unregister from application.")
-    @GetMapping("/unregister")
+    @DeleteMapping("/unregister")
     void unregister(@ApiIgnore @AuthenticationPrincipal final Account account) {
         accountService.removeAccount(account.getId());
     }
