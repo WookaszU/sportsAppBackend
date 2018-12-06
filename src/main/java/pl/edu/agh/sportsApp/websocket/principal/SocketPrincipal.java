8ea1e33dc772ec0663172e4f69a08df54bc4d6d4
WookaszU <l.uchman@interia.pl>
2018-12-06 04:00:18 +0100

    Bug fixes - event field usersIds is empty -> changes in socket rights inspector.

--- src/main/java/pl/edu/agh/sportsApp/websocket/principal/SocketPrincipal.java
+++ src/main/java/pl/edu/agh/sportsApp/websocket/principal/SocketPrincipal.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.sportsApp.websocket.principal;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -8,21 +9,32 @@ import lombok.experimental.FieldDefaults;
 import javax.security.auth.Subject;
 import java.security.Principal;
 
+@Builder
 @RequiredArgsConstructor
 @FieldDefaults(level= AccessLevel.PRIVATE, makeFinal = true)
 public class SocketPrincipal implements Principal {
 
-    String name;
+    String idAsString;
+
     @Getter
     Long id;
+
+    @Getter
+    String firstName;
+
+    @Getter
+    String lastName;
+
+    @Getter
+    String photoId;
 
     @Override
     public boolean implies(Subject subject) {
         return false;
     }
 
-    @Override
     public String getName() {
-        return name;
+        return idAsString;
     }
+
 }
